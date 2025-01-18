5e84149c840e9f7796fae54876ebd64f76071cf6
Eduardo Gallegos <eduardogallegos9@gmail.com>
2025-01-18 01:14:02 -0800

    Done with Wireshark copycat program!

--- trace.c
+++ trace.c
@@ -10,30 +10,47 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void TCP_print(const u_int8_t *data){
+char *get_port(uint16_t port) {
+    static char port_str[10];  // Static buffer to hold the port number string
+
+    if (port == 53) {
+        return "DNS";
+    }
+    else if (port == 80) {
+        return "HTTP";
+    }
+    else if (port == 443) {
+        return "HTTPS";
+    }
+    else {
+        sprintf(port_str, "%u", port);  // Convert port number to string
+        return port_str;
+    }
+}
+
+void TCP_print(const u_int8_t *data, const uint16_t total_length, const uint16_t IP_header_length){
     printf("\n\tTCP Header\n");
-    printf("\t\tSource Port:  %d\n", ntohs(*(uint16_t*)&data[34]));
-    printf("\t\tDest Port:  %d\n", ntohs(*(uint16_t*)&data[36]));
-    printf("\t\tSequence Number: %u\n", ntohl(*(uint32_t*)&data[38]));
-    printf("\t\tACK Number: %u\n", ntohl(*(u_int32_t*)&data[42]));
-    printf("\t\tData Offset (bytes): %u\n", ((data[46] & 0xf0) >> 4) * 4);
-    printf("\t\tSYN Flag: %s\n", get_yes_no(data[47] & 0x02));
-    printf("\t\tRST Flag: %s\n", get_yes_no(data[47] & 0x04));
-    printf("\t\tFIN Flag: %s\n", get_yes_no(data[47] & 0x01));
-    printf("\t\tACK Flag: %s\n", get_yes_no(data[47] & 0x10));
-    printf("\t\tWindow Size: %d\n", ntohs(*(uint16_t*)&data[48]));
+    uint16_t TCP_start = 13 + IP_header_length;
+    printf("\t\tSource Port:  %s\n", get_port(ntohs(*(uint16_t*)&data[TCP_start + 1])));
+    printf("\t\tDest Port:  %s\n", get_port(ntohs(*(uint16_t*)&data[TCP_start + 3])));
+    printf("\t\tSequence Number: %u\n", ntohl(*(uint32_t*)&data[TCP_start + 5]));
+    printf("\t\tACK Number: %u\n", ntohl(*(u_int32_t*)&data[TCP_start + 9]));
+    printf("\t\tData Offset (bytes): %u\n", ((data[TCP_start + 13] & 0xf0) >> 4) * 4);
+    printf("\t\tSYN Flag: %s\n", get_yes_no(data[TCP_start + 14] & 0x02));
+    printf("\t\tRST Flag: %s\n", get_yes_no(data[TCP_start + 14] & 0x04));
+    printf("\t\tFIN Flag: %s\n", get_yes_no(data[TCP_start + 14] & 0x01));
+    printf("\t\tACK Flag: %s\n", get_yes_no(data[TCP_start + 14] & 0x10));
+    printf("\t\tWindow Size: %d\n", ntohs(*(uint16_t*)&data[TCP_start + 15]));
     uint16_t *pseudoheader = TCP_pseudoheader(data);
 
     
-    uint16_t IP_total_length = ntohs(*(uint16_t*)&data[16]);
-    uint16_t IP_header_length = (data[14] & 0x0f) * 4;
-    uint16_t TCP_segment_length = IP_total_length - IP_header_length;
+    uint16_t TCP_segment_length = total_length - IP_header_length;
     uint16_t TCP_checksum_buffer_len = 12 + TCP_segment_length;
     uint16_t TCP_checksum_buffer[TCP_checksum_buffer_len];
     memcpy(&TCP_checksum_buffer, pseudoheader, 12);
-    memcpy(&TCP_checksum_buffer[6], &data[34], TCP_segment_length);
+    memcpy(&TCP_checksum_buffer[6], &data[TCP_start + 1], TCP_segment_length);
 
-    printf("\t\tChecksum: %s (0x%04x)\n", get_checksum((unsigned short*)&TCP_checksum_buffer, TCP_checksum_buffer_len), ntohs(*(uint16_t*)&data[50]));
+    printf("\t\tChecksum: %s (0x%04x)\n", get_checksum((unsigned short*)&TCP_checksum_buffer, TCP_checksum_buffer_len), ntohs(*(uint16_t*)&data[TCP_start + 17]));
 }
 
 
@@ -115,8 +132,11 @@ void header_print(pcap_t *ptr){
     uint16_t count = 1;
     while(pcap_next_ex(ptr, &header, &data) != PCAP_ERROR_BREAK){
 
+
         ethernet_print(ptr, count, data, header);
 
+        uint16_t total_length = ntohs(*(uint16_t*)&data[16]);
+        uint16_t IP_header_length = (data[14] & 0x0f) * 4;
         if(data[12] == 0x08 && data[13] == 0x00){
             printf("\t\tType: IP\n\n");
             IP_print(data);      // IP
@@ -129,15 +149,15 @@ void header_print(pcap_t *ptr){
         }
 
         if(data[23] == IPPROTO_ICMP){      // ICMP
-            ICMP_print(data);
+            ICMP_print(data, total_length, IP_header_length);
         }
 
         else if (data[23] == IPPROTO_TCP){    // TCP
-            TCP_print(data);
+            TCP_print(data, total_length, IP_header_length);
          }
 
         else if (data[23] == IPPROTO_UDP){   // UDP
-             UDP_print(data);
+             UDP_print(data, total_length, IP_header_length);
         }
 
         count++;
@@ -182,17 +202,18 @@ pcap_t *trace_init(char* program){
     return pointer;
 }
 
-void ICMP_print(const u_int8_t *data){
+void ICMP_print(const u_int8_t *data, const uint16_t total_length, const uint16_t IP_header_length){
     printf("\n\tICMP Header\n");
+    uint16_t ICMP_start = 13 + IP_header_length;
 
     if ( ( ((data[14] & 0xf0) >> 4) != 4) && (((data[14] & 0xf0) >> 4) != 6) ){
         printf("\t\tType: %d\n", 109);
     }
-    else if (data[34] == 0)
+    else if (data[ICMP_start + 1] == 0)
     {
        printf("\t\tType: %s\n", "Reply");
     }
-    else if(data[34] == 8){
+    else if(data[ICMP_start + 1] == 8){
         printf("\t\tType: %s\n", "Request");
     }
 
@@ -200,9 +221,10 @@ void ICMP_print(const u_int8_t *data){
 }
 
 
-void UDP_print(const u_int8_t *data){
-    uint16_t source_port = data[34] << 8 | data[35];
-    uint16_t destination_port = data[36] << 8 | data[37];
+void UDP_print(const u_int8_t *data, const uint16_t total_length, const uint16_t IP_header_length){
+    uint16_t UDP_start = 13 + IP_header_length;
+    uint16_t source_port = data[UDP_start + 1] << 8 | data[UDP_start + 2];
+    uint16_t destination_port = data[UDP_start + 3] << 8 | data[UDP_start + 4];
     printf("\n\tUDP Header\n");
     if(source_port == 53){
         printf("\t\tSource Port:  %s\n", "DNS");
@@ -210,7 +232,6 @@ void UDP_print(const u_int8_t *data){
     else{
         printf("\t\tSource Port:  %d\n", source_port);
     }
-
     if(destination_port == 53){
         printf("\t\tDest Port:  %s\n", "DNS");
     }
--- trace.h
+++ trace.h
@@ -16,17 +16,21 @@ void ethernet_print(pcap_t *ptr, uint16_t count, const u_int8_t *data, struct pc
 
 void IP_print(const u_int8_t *data);
 
-void TCP_print(const u_int8_t *data);
+void TCP_print(const u_int8_t *data,const uint16_t total_length,const uint16_t IP_header_length);
 
 uint16_t *TCP_pseudoheader(const u_int8_t *data);
 
-uint16_t *TCP_checksum_buffer(const u_int8_t *data, uint16_t *pseudoheader);
+uint16_t *TCP_checksum_buffer(const u_int8_t *data,uint16_t *pseudoheader);
 
-void UDP_print(const u_int8_t *data);
+void UDP_print(const u_int8_t *data,const uint16_t total_length,const uint16_t IP_header_length);
 
-void ICMP_print(const u_int8_t *data);
+void ICMP_print(const u_int8_t *data,const uint16_t total_length,const uint16_t IP_header_length);
+
+char *get_port(uint16_t port);
 
 void ARP_print(const u_int8_t *data);
+
+void print_port(uint16_t port);
 
 char *get_yes_no(uint16_t flag);
 
